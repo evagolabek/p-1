38b431ab14041d38fec6d657a9c0b1c8ceeda65e
evagolabek <golabek.eva@gmail.com>
2018-10-06 18:50:50 +0200

    add details for projects

--- script.js
+++ script.js
@@ -3,25 +3,33 @@
  var projects = [
   {
     name: "A-robot",
-    desciption: "altruistic_robot",
-    deadline: 2050
+    desciption: "Altruistic Robot",
+    deadline: 2050,
+    budget: '$ 3400 0000 000',
+    progress: "33%"
   },
   {
     name: "P-toilet",
-    desciption: "portable_toilet",
-    deadline: 2026
+    desciption: "Portable Toilet",
+    deadline: 2026,
+    budget: "$5400 0000",
+    progress: "50%"
   },
   {
     name: "T-catcher",
-    desciption: "thoughts_catcher",
-    deadline: 2077
+    desciption: "Thoughts Catcher",
+    deadline: 2077,
+    budget: "$ 10000000000",
+    progress: "17%"
   }
 ]
 
 var i;
 var list = function(projects) {
   for (i = 0; i < projects.length; i++) {
-    document.getElementById("projects-list").innerHTML += "<li><details><summary>" + projects[i].desciption + "</summary><p>" + projects[i].name + "</p><p>" + projects[i].deadline + "</p></details></li>";
+    document.getElementById("projects-list").innerHTML += "<li><details><summary>" + projects[i].desciption +
+    "</summary><p> name: " + projects[i].name + "</p><p> deadline: " + projects[i].deadline + "</p><p> progress: "+
+    projects[i].progress + "</p><p> budget: " + projects[i].budget + "</p></details></li>";
 
   }
 }
